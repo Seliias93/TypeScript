a9a9310f07606fff0b3c2b695e85ec0ccf2c06c9
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2023-02-06 11:15:14 -0300

    .

--- instanceof 2/instanceof-2.ts
+++ instanceof 2/instanceof-2.ts
@@ -1,6 +1,6 @@
 //Em JavaScript, Classes class são funções construtoras que geram objetos. Quando definimos uma classe, o TypeScript gera a interface do objeto produzido pela mesma.
 
-class Produto {
+class Produtos {
   nome: string;
   preco: number;
   constructor(nome: string, preco: number) {
@@ -11,18 +11,18 @@ class Produto {
     return `R$ ${this.preco}`;
   }
 }
-const livro = new Produto("A Guerra dos Tronos", 200);
+const livros = new Produto("A Guerra dos Tronos", 200);
 
 //Existem funções que retornam diferentes tipos de objetos. Com a palavra-chave instanceof podemos verificar se um objeto é uma instância (foi construído ou herda) de uma função construtora (class).
 
-class Livro {
+class Livros1 {
   autor: string;
   constructor(autor: string) {
     this.autor = autor;
   }
 }
 
-class Jogo {
+class game {
   jogadores: number;
   constructor(jogadores: number) {
     this.jogadores = jogadores;
@@ -39,11 +39,7 @@ function buscarProduto(busca: string) {
   return null;
 }
 
-const produtos = buscarProduto("O Hobbit");
-
-if (produtos instanceof Livro) {
-  produto.autor;
-}
+const Produto1 = buscarProduto("O Hobbit");
 
 //O instanceof verifica se a função construtora herda de outra (extends).
 
@@ -54,7 +50,7 @@ class Produto {
   }
 }
 
-class Livro extends Produto {
+class livro2 extends Produto {
   autor: string;
   constructor(nome: string, autor: string) {
     super(nome);
@@ -62,7 +58,7 @@ class Livro extends Produto {
   }
 }
 
-class Jogos extends Produto {
+class Jogo extends Produto {
   jogadores: number;
   constructor(nome: string, jogadores: number) {
     super(nome);
@@ -70,23 +66,29 @@ class Jogos extends Produto {
   }
 }
 
-//O instanceof é um operador que existe no JavaScript. Se você definir a interface de um objeto apenas com o interface e não possuir uma classe construtora do mesmo, não será possível utilizar o instanceof na interface
-
-interface Produto {
-  nome: string;
+function buscarProduto(busca: string) {
+  if (busca === "O Hobbit") {
+    return new Livro("O Hobbit", "J. R. R. Tolkien");
+  }
+  if (busca === "Dark Souls") {
+    return new Jogo("Dark Souls", 1);
+  }
+  return null;
 }
 
-const jogo: Produto = {
-  nome: "Dark Souls",
-};
+const produto1 = buscarProduto("O Hobbit");
+const produto3 = buscarProduto("Dark Souls");
+
+if (produto1 instanceof Produto) {
+  produto1.nome;
+}
+
+//O instanceof é um operador que existe no JavaScript. Se você definir a interface de um objeto apenas com o interface e não possuir uma classe construtora do mesmo, não será possível utilizar o instanceof na interface.
+
+interface Produto2 {
+  nome: string;
+}
 
 // Erro
 if (jogo instanceof Produto) {
 }
-
-//Exercício
-//1 - Selecione o link utilizando o método getElementById.
-
-//2 - Substitua o href do link (HTMLAnchorElement) de http:// para https://.
-
-//<a id="origamid" href="http://www.origamid.com">Origamid</a>
